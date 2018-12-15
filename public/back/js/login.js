93beee33178d0d25dec752a3aa10d271bf8427a9
zhangzhang <1392018097@qq.com>
2018-12-15 16:45:31 +0800

    登录完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -21,6 +21,9 @@ $(function() {
                         min:2,
                         max:6,
                         message:'用户名长度必须在2~6位之间'
+                    },
+                    callback:{
+                        message:'用户名错误!'
                     }
 
                 }
@@ -35,10 +38,46 @@ $(function() {
                         min:6,
                         max:12,
                         message:'密码长度必须在6~12位之间'
+                    },
+                    callback:{
+                        message:'密码错误!'
                     }
                 }
             }
         }
 
     })
+
+    $('#form').on('success.form.bv',function(e){
+        e.preventDefault();
+        //通过ajax提交
+        $.ajax({
+            type:'post',
+            data: $('#form').serialize(),
+            url: '/employee/employeeLogin',
+            dataType:'json',
+            success:function(info){
+                console.log(info)
+                //成功跳转到首页
+                if(info.success){
+                    location.href = 'index.html'
+                }
+                if(info.error === 1000){
+                    $('#form').data("bootstrapValidator").updateStatus("username", "INVALID", "callback");
+                }
+                if(info.error === 1001){
+                    $('#form').data("bootstrapValidator").updateStatus("password", "INVALID", "callback");
+                }
+                
+            }            
+            
+        })
+    })
+
+
+    //重置按钮
+    $('[type = "reset"]').click(function(){
+        $('#form').data("bootstrapValidator").resetForm();
+    })
+
 })
